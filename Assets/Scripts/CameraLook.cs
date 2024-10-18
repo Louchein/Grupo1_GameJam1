9401feecb6a16360bf02807d2cd7deee3ce7da2d
Daniel Ruiz <danielruiz2009@hotmail.com>
2024-10-18 14:36:47 -0500

    Se agrega movimiento del jugador

---
+++ Assets/Scripts/CameraLook.cs
@@ -0,0 +1,42 @@
+using System.Collections;
+using System.Collections.Generic;
+using System.Threading;
+using UnityEngine;
+
+public class CameraLook : MonoBehaviour
+{
+    float xRotation = 0;
+    public float mouseSensitivity = 80f;
+    public float anguloCam;
+    float mouseX, mouseY;
+
+    public Transform player;
+
+    void Start()
+    {
+        Cursor.lockState = CursorLockMode.Locked; // Bloquear el cursor en el centro de la pantalla
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        InputManager();
+        RotateCam();
+    }
+
+
+    void InputManager()
+    {
+        mouseX = Input.GetAxis("Mouse X") * mouseSensitivity * Time.deltaTime;
+        mouseY = Input.GetAxis("Mouse Y") * mouseSensitivity * Time.deltaTime;
+    }
+
+    void RotateCam()
+    {
+        xRotation -= mouseY;
+
+        xRotation = Mathf.Clamp(xRotation, -anguloCam, anguloCam);
+        transform.localRotation = Quaternion.Euler(xRotation, 0, 0);
+        player.Rotate(Vector3.up * mouseX);
+    }
+}
---
+++ Assets/Scripts/PlayerController.cs
@@ -0,0 +1,47 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour
+{
+    CharacterController controller;
+
+    // Coordenadas de movimiento
+    Vector3 movement;
+    float movZ, movX;
+    public float speed;
+
+
+    void Start()
+    {
+        controller = GetComponent<CharacterController>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        InputManager();
+        MovePlayer();
+    }
+
+    void InputManager()
+    {
+        // Se obtiene el axis para mover el personaje 
+        movZ = Input.GetAxis("Vertical");
+        movX = Input.GetAxis("Horizontal");
+    }
+
+
+    void MovePlayer()
+    {
+        // Combina los vectores de movimiento vertical y horizontal
+        Vector3 move = transform.forward * movZ + transform.right * movX;
+        move = move.normalized * speed; // Normaliza el vector y aplica la velocidad
+
+        // Se aplica gravedad
+        move.y += Physics.gravity.y * Time.deltaTime;
+
+        // Mover el personaje
+        controller.Move(move * Time.deltaTime);
+    }
+}
